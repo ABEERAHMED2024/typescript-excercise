5716b627dce14287543f8167d696200dffee2702
SYED ABEER AHMED <160941514+ABEERAHMED2024@users.noreply.github.com>
2024-04-12 17:51:53 -0700

    updated

--- excercise45/main.js
+++ excercise45/main.js
@@ -1,20 +1,14 @@
-function car_info(manufacturer, model) {
+function make_car(manufcture, model) {
     var options = [];
     for (var _i = 2; _i < arguments.length; _i++) {
         options[_i - 2] = arguments[_i];
     }
-    var car = {
-        "manufacturer": manufacturer,
-        "model": model
-    };
-    // Adding additional options
-    for (var _a = 0, options_1 = options; _a < options_1.length; _a++) {
-        var option = options_1[_a];
-        Object.assign(car, option);
-    }
-    return car;
+    var car = { manufcture: manufcture, model: model };
+    options.forEach(function (_a) {
+        var key = _a[0], value = _a[1];
+        return car[key] = value;
+    });
+    return (car);
 }
-// Call the function with required information and additional options
-var car = car_info("Toyota", "Camry", { "color": "red" }, { "year": 2022 });
-// Print the Object returned by the function
-console.log(car);
+console.log(make_car("toyota", "corola", ["color", "red"], ["year", "2020"]));
+console.log(make_car("ford", "fiesta"), ["color", "blue"], ["sunroof", "true"]);
--- excercise45/main.ts
+++ excercise45/main.ts
@@ -1,19 +1,7 @@
-function car_info(manufacturer: string, model: string, ...options: Record<string, any>[]): Record<string, any> {
-    let car: Record<string, any> = {
-        "manufacturer": manufacturer,
-        "model": model
-    };
-
-    // Adding additional options
-    for (let option of options) {
-        Object.assign(car, option);
-    }
-
-    return car;
+function make_car(manufcture:string,model:string, ...options:[string,string][]):object {
+    let car = {manufcture,model};
+    options.forEach(([key,value]) => car[key]= value);
+    return(car);
 }
-
-// Call the function with required information and additional options
-let car = car_info("Toyota", "Camry", { "color": "red" }, { "year": 2022 });
-
-// Print the Object returned by the function
-console.log(car);
+console.log(make_car("toyota","corola",["color","red"],["year","2020"]));
+console.log(make_car("ford","fiesta"),["color","blue"],["sunroof","true"]);
